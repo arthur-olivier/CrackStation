2a473a7600e584fda1c82aa0ee93cc83f3eb1b2e
arthurolivier1 <arthurolivier69@gmail.com>
2022-11-17 17:05:06 -0800

    final_commit_17_11

--- Sources/CrackStation/Decrypter.swift
+++ Sources/CrackStation/Decrypter.swift
@@ -1,6 +1,20 @@
+//
+// SI Lab, 2022
+//
 
-public protocol Decrypter {
-    init()
-    func decrypt(shaHash: String) -> String?
+public struct CrackStation: Decrypter {
+    private let lookuptable : [String : String]
+    
+    public init() {
+        do {
+            lookuptable = try GetLookUpTable().loadDictionaryFromDisk()
+        }
+        catch{ lookuptable = ["":""]}
+    }
+
+    public func decrypt(shaHash: String) -> String? {
+        let password = lookuptable[shaHash]
+        return password
+    }
 }
 
